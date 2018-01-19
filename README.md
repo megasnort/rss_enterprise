0d926678df3975b813bfb3d70eb57581c0d02e30
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2018-01-19 12:50:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# RSS Enterprise
+
+In my opinion, existing RSS Readers make everything to complicated. I just want to
+
+- subscribe to feeds
+- view one list with the unread news items of all the feeds
+- view the body of these news item by walking this list
+- open the news item in the default browser, in the background
+
+Older versions of NetNewsWire were able to provide this functionality but since I moved to linux I no longer have that choice.
+
+The name? Trekkies will find it hilarious. That is reason enough.
+
---
+++ rss_enterprise.py
@@ -0,0 +1,43 @@
+import os
+import json
+import feedparser
+
+from future import Future
+
+
+def get_settings_path():
+    return os.path.join('test', 'test_settings.json')
+
+
+def load_settings(settings_path):
+    print('loading settings')
+    with open(settings_path) as fp:
+        settings = json.load(fp)
+        return settings
+
+
+def load_news_items(feed_urls):
+    print('loading news items')
+    future_calls = [Future(feedparser.parse, rss_url) for rss_url in feed_urls]
+    feeds = [future_obj() for future_obj in future_calls]
+
+    entries = []
+
+    for feed in feeds:
+        entries.extend(feed["items"])
+
+    sorted_entries = sorted(entries, key=lambda entry: entry['published'])
+
+    return sorted_entries
+
+
+def main():
+    settings_path = get_settings_path()
+    settings = load_settings(settings_path)
+    news_items = load_news_items(settings['feed_urls'])
+    for i in news_items:
+        print(i['title'])
+
+
+if __name__ == '__main__':
+    main()
