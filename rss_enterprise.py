adf81479e19514f6571732c8b911ef5f69b1ae27
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2018-01-20 13:21:54 +0100

    Make it a class for easier data sharing also add event for printing clicked content

--- rss_enterprise.py
+++ rss_enterprise.py
@@ -7,71 +7,76 @@ import feedparser
 from future import Future
 
 
-def get_settings_path():
-    return os.path.join('test', 'test_settings.json')
+class RssEnterprise():
+    def __init__(self):
+        root = tk.Tk()
+        root.geometry("800x500")
 
+        top_frame = Frame(root)
+        top_frame.pack(fill=BOTH, expand=True)
 
-def load_settings(settings_path):
-    print('loading settings')
-    with open(settings_path) as fp:
-        settings = json.load(fp)
-        return settings
+        bottom_frame = Frame(root)
+        bottom_frame.pack(fill=BOTH, expand=True)
 
+        self.tree = ttk.Treeview(top_frame, selectmode='browse')
+        self.refresh_list()
 
-def load_news_items(feed_urls):
-    print('loading news items')
-    future_calls = [Future(feedparser.parse, rss_url) for rss_url in feed_urls]
-    feeds = [future_obj() for future_obj in future_calls]
+        scrollbar_tree = ttk.Scrollbar(top_frame, orient="vertical", command=self.tree.yview)
+        self.tree.configure(yscrollcommand=scrollbar_tree.set)
+        self.tree.bind("<<TreeviewSelect>>", self.on_tree_select)
 
-    entries = []
+        scrollbar_tree.pack(side='right', fill='y')
+        self.tree.pack(fill=BOTH, expand=True)
 
-    for feed in feeds:
-        entries.extend(feed["items"])
+        text = Text(bottom_frame)
 
-    sorted_entries = sorted(entries, key=lambda entry: entry['published'])
+        scrollbar_text = ttk.Scrollbar(bottom_frame, orient="vertical", command=text.yview)
+        text.configure(yscrollcommand=scrollbar_text.set)
+        scrollbar_text.pack(side='right', fill='y')
+        text.pack(fill=BOTH, expand=True)
 
-    return sorted_entries
+        root.mainloop()
 
+    def get_settings_path(self):
+        return os.path.join('test', 'test_settings.json')
 
-def refresh_list(tree):
-    settings_path = get_settings_path()
-    settings = load_settings(settings_path)
-    news_items = load_news_items(settings['feed_urls'])
-    for i in news_items:
-        # print(i)
-        try:
-            tree.insert('', 'end', i['id'], text=i['title'])
-        except TclError:
-            pass
+    def load_settings(self, settings_path):
+        print('loading settings')
+        with open(settings_path) as fp:
+            settings = json.load(fp)
+            return settings
 
+    def load_news_items(self, feed_urls):
+        print('loading news items')
+        future_calls = [Future(feedparser.parse, rss_url) for rss_url in feed_urls]
+        feeds = [future_obj() for future_obj in future_calls]
 
-def main():
-    root = tk.Tk()
-    root.geometry("800x500")
+        entries = []
 
-    top_frame = Frame(root)
-    top_frame.pack(fill=BOTH, expand=True)
+        for feed in feeds:
+            entries.extend(feed["items"])
 
-    bottom_frame = Frame(root)
-    bottom_frame.pack(fill=BOTH, expand=True)
+        sorted_entries = sorted(entries, key=lambda entry: entry['published'])
 
-    tree = ttk.Treeview(top_frame, selectmode='browse')
-    refresh_list(tree)
+        return sorted_entries
 
-    scrollbar_tree = ttk.Scrollbar(top_frame, orient="vertical", command=tree.yview)
-    tree.configure(yscrollcommand=scrollbar_tree.set)
-    scrollbar_tree.pack(side='right', fill='y')
-    tree.pack(fill=BOTH, expand=True)
+    def refresh_list(self):
+        settings_path = self.get_settings_path()
+        settings = self.load_settings(settings_path)
+        news_items = self.load_news_items(settings['feed_urls'])
+        for i in news_items:
+            # print(i)
+            try:
+                self.tree.insert('', 'end', i['id'], text=i['title'])
+            except TclError:
+                pass
 
-    text = Text(bottom_frame)
-
-    scrollbar_text = ttk.Scrollbar(bottom_frame, orient="vertical", command=text.yview)
-    text.configure(yscrollcommand=scrollbar_text.set)
-    scrollbar_text.pack(side='right', fill='y')
-    text.pack(fill=BOTH, expand=True)
-
-    root.mainloop()
+    def on_tree_select(self, event):
+        print("selected items:")
+        for item in self.tree.selection():
+            item_text = self.tree.item(item, "text")
+            print(item_text)
 
 
 if __name__ == '__main__':
-    main()
+    rss_enterprise = RssEnterprise()
